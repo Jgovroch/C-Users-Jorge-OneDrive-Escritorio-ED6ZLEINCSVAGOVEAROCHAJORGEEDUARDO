6c74026d921127aee7735cc3fcf1ab0fef1e977e
Jgovroch <je.govea.r@edu.utc.mx>
2022-05-25 13:12:25 -0500

    EjemploWhile

--- 02Sentencias/EjemploSentencias.cpp
+++ 02Sentencias/EjemploSentencias.cpp
@@ -2,13 +2,23 @@
 
 using namespace std;
 
-int main(){
+*//int main(){
 	//uso de sentencias de control
 	//do while
-	int i=1;
-	do{
+*//	int i=1;
+*//	do{
 		//vamos a crear un programa que imprima hasta el 10
-		cout<<i<<endl;
-		i++;
-	}while(i<11);
+*//		cout<<i<<endl;
+*//		i++;
+*//	}while(i<11);
+*//}
+
+//while
+int cont = 1;
+while(cont <=10){
+	cout<<cont<<end;
+	cont++;
+}cout<<"Se acabo";
+
 }
+
